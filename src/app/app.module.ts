5ffaa32410afc539bca7a07e7f4281347c178ca3
Saravanan <saravanang@syncfusion.com>
2018-11-26 12:14:16 +0530

    Add files via upload
    Angular 7 sample files moved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
-import { BrowserModule, } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { RichTextEditorAllModule } from '@syncfusion/ej2-angular-richtexteditor';
 import { NgModule } from '@angular/core';
-import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +9,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule, FormsModule, RichTextEditorAllModule
+    BrowserModule, RichTextEditorAllModule
   ],
 
   bootstrap: [AppComponent]
